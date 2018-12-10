0cbc8a8e5fa7369c5e64e05856a0ae7ef051446a
yuqihui <mg5851@126.com>
2018-12-10 18:44:08 +0800

    3

--- fisher.py
+++ fisher.py
@@ -1,21 +1,32 @@
 from flask import Flask,make_response
-
+from helper import is_isbn_or_key
+from yushu_book import YuShuBook
+import json
+from flask import jsonify
 
 __author__ = 'mango'
 app = Flask(__name__)
 app.config.from_object('config')
 
-@app.route('/book/search')    #通过装饰器给hello函数 一个路由
-def search():
-    """
-    q
-    start
-    count
-    isbn
+@app.route('/book/search/<q>/<page>')    #通过装饰器给hello函数 一个路由
+def search(q,page):
+    #以下代码是用来判断q是isbn还是关键字
+    isbn_or_key = is_isbn_or_key(q)
+    if isbn_or_key == 'isbn':
+        result = YuShuBook.search_by_isbn(q)
+    else:
+        result = YuShuBook.search_by_keyword(q)
+        #难点：路由（url）的设计
+        #dict 序列化
+        #API + JS(单页面，NG、VUE、React 前后端分离 SEO
+        #TPS + 小程序 + webview
+        #网站 多页面 ajax
+        #对立
+        # C GO
+        #API
+    return jsonify(result)
+    # return json.dumps(result),200,{'content-type':'application/json'}
 
-    """
-
-    pass
 def helloo():
     return 'hello,mango ...'
 
--- http.py
+++ http.py
@@ -3,21 +3,14 @@
 
 from urllib import request
 import requests
+#爬虫 scrapy ,requests + beautiful soap来处理内容
 class HTTP:
-    def get(self,url,return_json=True):
+    @staticmethod
+    def get(url,return_json=True):
         r = requests.get(url)
         if r.status_code != 200:
             return {} if return_json else''
         return r.json() if return_json else r.text
-        # if r.status_code == 200:
-        #     if return_json:
-        #         return r.json()
-        #     else:
-        #         return r.text
-        # else:
-        #     if return_json:
-        #         return {}
-        #     else:
-        #         return ''
+
 
 
